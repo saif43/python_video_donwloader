515b1e53f480e87dda95d4b583be8b35155d8382
saif43 <saif.ahmed.anik.0@gmail.com>
2019-12-17 13:25:22 +0600

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pytube
-video_url = 'https://www.youtube.com/watch?v=e4pOJmdLnVM' # paste here your Youube videos' url
+
+video_url = input('Enter Youtube video link: ') # paste here your Youube videos' url
 youtube = pytube.YouTube(video_url)
 video = youtube.streams.first()
 video.download('')
